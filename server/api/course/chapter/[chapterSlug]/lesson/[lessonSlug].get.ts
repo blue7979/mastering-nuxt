f347bb8ac577bdf321dbb148f97ad1405473fdb7
blue <blue@next1.ph>
2023-09-06 04:13:08 +0800

    5-12 prisma basic

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,36 +1,23 @@
-import { QueryChapterLesson } from '~/types/Queries';
-import {
-  Chapter, Course, LessonWithPath,
-} from '~/types/Course';
-import course from '~/server/courseData';
+import { PrismaClient } from '@prisma/client';
 
-course as Course;
+const prisma = new PrismaClient();
 
-// "server/routes/api === server/api" is same address
-export default defineEventHandler((event): LessonWithPath => {
-  const { chapterSlug, lessonSlug } = event.context.params as QueryChapterLesson;
+export default defineEventHandler(async (event) => {
+  const { chapterSlug, lessonSlug } = event.context.params;
 
-  const chapter: Maybe<Chapter> = course.chapters.find(
-    (c) => c.slug === chapterSlug,
-  );
-
-  if (!chapter) {
-    throw createError({
-      statusCode: 404,
-      message: 'Chapter not found',
-    });
-  }
-
-  const lesson = chapter.lessons.find(
-    (l) => l.slug === lessonSlug,
-  );
-
-  if (!lesson) {
-    throw createError({
-      statusCode: 404,
-      message: 'Lesson not found',
-    });
-  }
-
-  return { ...lesson, path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}` };
+  return prisma.lesson.findMany({
+    where: {
+      Chapter: {
+        slug: chapterSlug,
+      },
+    },
+    include: {
+      Chapter: {
+        select: {
+          slug: true,
+          title: true,
+        },
+      },
+    },
+  });
 });
